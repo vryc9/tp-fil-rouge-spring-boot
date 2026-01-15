883fa1d274bcac17b38838401dffd662b3e9fdd3
Enzo Volpato <enzo@MacBook-Air-de-Enzo.local>
2026-01-15 15:08:08 +0100

    feat: add entity

---
+++ src/main/java/edu/esiea/tp_fil_rouge/domain/models/Lesson.java
@@ -0,0 +1,49 @@
+package edu.esiea.tp_fil_rouge.domain.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+
+    private Theme theme;
+
+    public Lesson(){}
+
+    public Lesson(String title, Theme theme) {
+        this.title = title;
+        this.theme = theme;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+}
---
+++ src/main/java/edu/esiea/tp_fil_rouge/domain/models/Theme.java
@@ -0,0 +1,52 @@
+package edu.esiea.tp_fil_rouge.domain.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Theme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private float coef;
+
+
+    private String name;
+
+
+    public Theme() {
+    }
+
+    public Theme(float coef, String name) {
+        this.coef = coef;
+        this.name = name;
+    }
+
+    public float getCoef() {
+        return coef;
+    }
+
+    public void setCoef(float coef) {
+        this.coef = coef;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
