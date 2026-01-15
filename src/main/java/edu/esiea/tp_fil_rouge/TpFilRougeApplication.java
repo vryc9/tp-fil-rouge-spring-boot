a2cd524a66fb4bbc37a91588fe86774519c3c7ad
vryc9 <enzo47volpato@gmail.com>
2026-01-15 11:43:15 +0100

    feat: 🎸 init

---
+++ src/main/java/edu/esiea/tp_fil_rouge/TpFilRougeApplication.java
@@ -0,0 +1,13 @@
+package edu.esiea.tp_fil_rouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFilRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFilRougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/esiea/tp_fil_rouge/TpFilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esiea.tp_fil_rouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
